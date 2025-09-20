ff9408ed8beff8f470037cf197060a0c26c839a6
David Hernandez <david.hernandez@example.com>
2025-09-20 01:39:11 -0500

    chore(Santiago): rama vacía (placeholder README); lista para trabajo del integrante

--- src/server.js
+++
@@ -1,9 +0,0 @@
-﻿require("dotenv").config();
-const app = require("./app");
-const connectDB = require("./config/database");
-const PORT = process.env.PORT || 3000;
-
-(async () => {
-  await connectDB();
-  app.listen(PORT, ()=>console.log(`Server up: http://localhost:${PORT}`));
-})();
